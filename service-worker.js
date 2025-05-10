a3f7c5e4ec758d5ebb99defcbd4ebab94e4f7d32
mbi-7 <mbialghaffari@gmail.com>
2025-05-10 23:51:43 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('g6pd-meal-cache').then(function(cache) {
       return cache.addAll([
-        './G6PD_Safe_Web_Plan_Dark_Mode_With_Tracker.html',
+        './index.html',
         './manifest.json',
         './icon-192.png',
         './icon-512.png'
